1987cb849c879bd726d97e981c3f4bdaf5d3938e
Dilshad Hussain Shadbalti <shadbalti2@gmail.com>
2025-11-06 14:30:26 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     <div className="container py-8 md:py-12">
       <div className="mx-auto max-w-5xl space-y-8">
         <div className="space-y-2 text-center">
-         <h1 className="text-4xl sm:text-6xl font-bold mb-4"><span className="text-blue-500">OpenBalti </span>Dictionary</h1>
+         <h1 className="text-4xl sm:text-6xl font-bold mb-4"><span className="text-blue-800">OpenBalti </span>Dictionary</h1>
           <p className="mx-auto max-w-[700px] text-muted-foreground md:text-lg">
             Explore and contribute to the digital preservation of the Balti language
           </p>
